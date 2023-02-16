583a7ff6f145b25ab647a87ff613d79a93561f17
kubocker <kubocker@gmail.com>
2023-02-17 08:04:28 +0900

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,22 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const appId = 'game-platform.ionic.io';
+const appName = 'game-platform';
+const server = process.argv.includes('-hmr') ? {
+  'url': 'http://100.115.92.203:5173',   // always have http:// in url
+  'cleartext': true
+} : {};
+const webDir = 'build';
+
+const config: CapacitorConfig = {
+  appId,
+  appName,
+  webDir,
+  server
+};
+
+if (process.argv.includes('-hmr')) console.log('WARNING: running capacitor with livereload config', config);
+
+export default config;
+  
